834869237ef7d1a3202ec6326450d9913f2b6819
k1ycee <chiagozieani145@gmail.com>
2021-11-24 16:28:34 +0100

    Final working code

--- src/com/company/Quiz.java
+++ src/com/company/Quiz.java
@@ -11,24 +11,29 @@ public class Quiz {
         String[] options = {"A", "B", "C", "D", "E"};
         question += "A. Islamabad \n B. Abuja \n C. Kuala Lumpur \n D. Moscow \n E. Ulan Bator";
 
-        String answer = JOptionPane.showInputDialog(question);
-        answer = answer.toUpperCase();
 
-//        while (answer.equals("A")){
-//
-//                break;
-//        }
-        if(answer.equals("A")){
-            JOptionPane.showMessageDialog(null, "Correct!");
+
+        while (true){
+            String answer = JOptionPane.showInputDialog(question);
+            answer = answer.toUpperCase();
+
+            if(answer.equals("A")){
+                JOptionPane.showMessageDialog(null, "Correct!");
+                break;
+            }
+            else if(answer.equals("B") || answer.equals("C") || answer.equals("D") || answer.equals("E")){
+                JOptionPane.showMessageDialog(null, "Incorrect. Please try again.");
+            }
+            else {
+
+                JOptionPane.showMessageDialog(null, "Invalid answer. Please enter A, B, C, D, or E.");
+            }
+
+
+
         }
-        else if(!answer.equals("B") || !answer.equals("C") || !answer.equals("D") || !answer.equals("E")){
-            JOptionPane.showMessageDialog(null, "Invalid answer. Please enter A, B, C, D, or E.");
-        } else {
-
-            JOptionPane.showMessageDialog(null, "Incorrect. Please try again.");
 
 
-        }
 
     }
 }
