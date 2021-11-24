634561578ed116ab8c36eebf0ee2133118f15b7f
k1ycee <chiagozieani145@gmail.com>
2021-11-24 05:35:03 +0100

    To add final looping in next iteration

--- src/com/company/Quiz.java
+++ src/com/company/Quiz.java
@@ -1,4 +1,34 @@
 package com.company;
+import javax.swing.JOptionPane;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 public class Quiz {
+
+    public static void main(String[] args){
+        String question = "What is the capital of Pakistan?\n";
+        String[] options = {"A", "B", "C", "D", "E"};
+        question += "A. Islamabad \n B. Abuja \n C. Kuala Lumpur \n D. Moscow \n E. Ulan Bator";
+
+        String answer = JOptionPane.showInputDialog(question);
+        answer = answer.toUpperCase();
+
+//        while (answer.equals("A")){
+//
+//                break;
+//        }
+        if(answer.equals("A")){
+            JOptionPane.showMessageDialog(null, "Correct!");
+        }
+        else if(!answer.equals("B") || !answer.equals("C") || !answer.equals("D") || !answer.equals("E")){
+            JOptionPane.showMessageDialog(null, "Invalid answer. Please enter A, B, C, D, or E.");
+        } else {
+
+            JOptionPane.showMessageDialog(null, "Incorrect. Please try again.");
+
+
+        }
+
+    }
 }
