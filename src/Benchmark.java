6a9ea69b7676fcd47ca7ad40bdb52cafe71837de
k1ycee <chiagozieani145@gmail.com>
2022-09-07 03:51:16 +0100

    Week 1 assignment

--- src/Benchmark.java
+++ src/Benchmark.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class Benchmark {
+import java.util.Arrays;
+
+/**
+ * This class contains a methods for performing a proper insertion sort
+ * a method to benchmark the speed of sorting between the insertion sort
+ * and the speed of Arrays.sort()
+ * @author ThankGod Chiagozie
+
+ */
+public class Benchmark {
+
+    public static void main(String [] args) {
+        checker();
+    }
+
+
+    public static void checker(){
+        final int arraySize = 100000; // array size constant
+        int[] firstList = new int[arraySize];
+        // Populate first list array with max integer values
+        for(int i = 0; i < arraySize; i++){
+            firstList[i] = (int)(Integer.MAX_VALUE * Math.random());
+        }
+
+        int[] secondList = firstList; // make second array equal to the first
+        long startTime = System.currentTimeMillis(); // start timer
+        insertionSort(secondList);
+        long runTime = System.currentTimeMillis() - startTime; // calculate runtime
+
+        /**
+         *
+         * Insertion Sort benchmarks in seconds for different array sizes
+         *  1000 = 0.007s
+         *  10000 = 0.024s
+         *  100000 = 0.791s
+         */
+        System.out.println(runTime/1000.0);
+
+        long startTimeSecondList = System.currentTimeMillis(); /// start timer second array
+        Arrays.sort(firstList);
+        long runTimeSecondList = System.currentTimeMillis() - startTimeSecondList; // calculate runtime for second array
+        /**
+         *
+         * Arrays.sort() benchmarks in seconds for different array sizes
+         *  1000 = 0.0s
+         *  10000 = 0.001s
+         *  100000 = 0.003s
+         */
+        System.out.println(runTimeSecondList/1000.0);
+    }
+
+
+
+    static void insertionSort(int[] A) {
+        // Sort the array A into increasing order.
+        int itemsSorted; // Number of items that have been sorted so far.
+        for (itemsSorted = 1; itemsSorted < A.length; itemsSorted++) {
+            // Assume that items A[0], A[1], ... A[itemsSorted-1]
+            // have already been sorted.  Insert A[itemsSorted]
+            // into the sorted part of the list.
+            int temp = A[itemsSorted];  // The item to be inserted.
+            int loc = itemsSorted - 1;  // Start at end of list.
+            while (loc >= 0 && A[loc] > temp) {
+                A[loc + 1] = A[loc]; // Bump item from A[loc] up to loc+1.
+                loc = loc - 1;       // Go on to next location.
+            }
+            A[loc + 1] = temp; // Put temp in last vacated space.
+        }
+    }
+
 }
--- src/IOExperiment.java
+++ src/IOExperiment.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class IOExperiment {
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * This class contains a methods that takes a user input
+ * userUrl and userfilenam, to scrape the web for the userUrl and put the result in
+ * userfilename using I/O in java
+ * @author ThankGod Chiagozie
+
+ */
+
+public class IOExperiment {
+
+
+    public static void main(String [] args) {
+//      Initialize input and output streams
+        InputStream in;
+        OutputStream out;
+
+        Scanner userUrl = new Scanner(System.in);  // Collect user url
+        System.out.println("Enter url:");
+        String urlString = userUrl.nextLine();
+        Scanner userFile = new Scanner(System.in);  // Collect filename for document from user
+        System.out.println("Enter fileName:");
+        String fileString = userFile.nextLine();
+        try {
+            URL url = new URL("https://" + urlString); // make url object
+            in = url.openStream(); // pass result to the `in` variable
+            out = new FileOutputStream(fileString); // pass [FileOutputStream] to the `out` variable
+            copyStream(in, out);
+            in.close();
+            out.close();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void copyStream(InputStream in, OutputStream out)
+            throws IOException {
+        int oneByte = in.read();
+        while (oneByte >= 0) { // negative value indicates end-of-stream
+            out.write(oneByte);
+            oneByte = in.read();
+        }
+    }
 }
